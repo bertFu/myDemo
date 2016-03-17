e989bc66f43dde6dfd8f6a49aa83419066d58aad
bert <429874664@qq.com>
2016-03-17 13:37:42 +0800

    第二天，理解scoket机制

--- app.js
+++ app.js
@@ -25,15 +25,39 @@ var io = require('socket.io').listen(app.listen(port))
     就会产生一个socket对象，
     使用这个对象，我们就可以和对应的客户端实时通信了
 */
-var messages = []
+var messages = [] // `messages数组` 暂时存储客户端上传上来的数据
 
 io.sockets.on('connection', function (socket) {
     // socket.emit('connected')
+    
+    /*
+        用户连上来后，
+        向服务端发送getAllMessages请求，
+        获取所有消息，
+        服务器就把所有的消息通过allMessages事件推送给客户端；
+    */
     socket.on('getAllMessages', function () {
+        /* 
+            服务端：发出 `allMessage` 事件，将 `messages数组` 信息发送给客户端
+            客户端：接收 `allMessage` 事件，对 `messages数组` 信息遍历 `messages数组` 得到对应信息渲染模板
+        */
+        console.log('客户端链接上来时触发！！   ' + messages);
         socket.emit('allMessages', messages)
     })
+    
+    /*
+        当用户创建消息时，
+        向服务端发送createMessage事件，
+        服务端把消息存放到messages数组中，
+        并向所有的客户端广播messageAdded，有新的消息添加进来。
+    */
     socket.on('createMessage', function (message) {
-        messages.push(message)
+        messages.push(message) // 将客户端发送 `message` 信息添加到 `messages数组` 中.
+        console.log('客户端发送信息时触发！！   ' + messages);
+        /*
+            服务端：发出 `messageAdded` 事件，传入新增的 `message` 信息。
+            客户端：接受 `messageAdded` 事件，对他做什么事情呢？同样的，客户端将 `message` 信息添加到 `$scope.messages数组` 中。
+        */
         io.sockets.emit('messageAdded', message)
     })
 })
--- static/technode.js
+++ static/technode.js
@@ -26,10 +26,18 @@ angular.module('techNodeApp').factory('socket', function($rootScope) {
 
 angular.module('techNodeApp').controller('RoomCtrl', function($scope, socket) {
   $scope.messages = []
-  socket.emit('getAllMessages')
-  socket.on('allMessage', function (messages) {
+  socket.emit('getAllMessages') // 告诉服务端，我要和你链接了。
+  
+  /*
+    获取服务端当前的聊天信息 `messages`
+  */
+  socket.on('allMessages', function (messages) {
     $scope.messages = messages
   })
+  
+  /*
+    获取服务端返回的最新信息添加到 `messages` 数组中
+  */
   socket.on('messageAdded', function (message) {
     $scope.messages.push(message)
   })
@@ -41,11 +49,18 @@ angular.module('techNodeApp').controller('MessageCreatorCtrl', function($scope, 
     if ($scope.newMessage == '') {
       return
     }
+    
+    /*
+        接受用户输入的信息，发送给服务端
+    */
     socket.emit('createMessage', $scope.newMessage)
     $scope.newMessage = ''
   }
 })
 
+/* 
+    监听模型信息变化，将滚动条移动至最底部，以便能查询到最新显示的信息 
+*/
 angular.module('techNodeApp').directive('autoScrollToBottom', function() {
   return {
     link: function(scope, element, attrs) {
@@ -63,6 +78,9 @@ angular.module('techNodeApp').directive('autoScrollToBottom', function() {
   };
 });
 
+/*
+     在textarea回车，默认会换行，使用这个组件，可以通过ctrl+enter来换行，而enter则触发绑定的行为，在这里就是createMessage这个方法
+*/
 angular.module('techNodeApp').directive('ctrlEnterBreakLine', function() {
   return function(scope, element, attrs) {
     var ctrlDown = false
